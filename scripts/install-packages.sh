5ed3e982921e6c12c9ecf5e30985cd877c37b7b5
Guille Polito <guillermopolito@gmail.com>
2017-10-20 17:37:33 +0200

    added initial travis setup

---
+++ scripts/install-packages.sh
@@ -0,0 +1,15 @@
+./pharo Pharo.image eval "
+Metacello new 
+	baseline: 'Garage';
+	repository: 'github://pharo-rdbms/garage';
+	load: '${1}'.
+Metacello new 
+    baseline: 'Garage';
+    repository: 'github://pharo-rdbms/glorp';
+    load.
+Metacello new 
+	baseline: 'GarageGlorp';
+	repository: 'filetree://.';
+	load: '${1}'.
+Smalltalk snapshot: true andQuit: true.
+"
---
+++ scripts/setup.sh
@@ -0,0 +1,20 @@
+case $1 in
+sqlite3)
+  ./pharo Pharo.image eval --save "GAGlorpContinuousIntegrationConfiguration new configureSqlite3"
+  ;;
+mysql)
+  mysql -uroot -e "CREATE DATABASE sodbxtest /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+  mysql -uroot -e "CREATE USER sodbxtest@localhost IDENTIFIED BY 'sodbxtest';"
+  mysql -uroot -e "GRANT ALL PRIVILEGES ON sodbxtest.* TO 'sodbxtest'@'localhost';"
+  mysql -uroot -e "FLUSH PRIVILEGES;"
+  ./pharo Pharo.image eval --save "GAGlorpContinuousIntegrationConfiguration new configureMysql"
+  ;;
+postgresV2)
+  psql -c "create user sodbxtest createdb createuser password 'sodbxtest';" -U postgres;
+  psql -c "alter user postgres password 'postgres';" -U postgres;
+  ./pharo Pharo.image eval --save "GAGlorpContinuousIntegrationConfiguration new configurePostgresql";
+  ;;
+*)
+  echo "Unsupported database $1"
+  ;;
+esac
